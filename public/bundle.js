73e9d69991674cc18996fc9dc9ae0aeee6afc40c
Sushmitha C G <sushmithacg.20@gmail.com>
2020-06-10 17:37:58 +0530

    initial commit

--- public/bundle.js
+++ public/bundle.js
@@ -8171,49 +8171,42 @@ function config (name) {
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}],34:[function(require,module,exports){
-//import simple-peer package
 let Peer = require('simple-peer');
-//in client side js,we can't use require(), so we use watchify
-let socket = io();  //directly connect to our host
-const video = document.querySelector('video');  //reference to our own video
+let socket = io();  
+const video = document.querySelector('video');  
 const filter = document.querySelector('#filter');
 const checkboxTheme = document.querySelector('#theme');
-let client = {}; //related to other person
+let client = {}; 
 let currentFilter;
 
-//get video stream
-//ask browser for user permission
-navigator.mediaDevices.getUserMedia({video: true, audio: true}) //get video/voice stream
-//if user gives permission,
+navigator.mediaDevices.getUserMedia({video: true, audio: true})
 .then(stream => {
-    //notify backend to add one client
-    socket.emit('NewClient');
-    video.srcObject = stream; //Received a video stream, which can be displayed in a video tag
+    socket.emit('NewClient'); 
+    video.srcObject = stream; 
     video.play();
 
-    //when u change the filter
     filter.addEventListener('change', (event) => {
         currentFilter = event.target.value;
-        video.style.filter = currentFilter;  //this is for our own video
-        SendFilter(currentFilter); //this is for other person
+        video.style.filter = currentFilter;  
+        SendFilter(currentFilter); 
         event.preventDefault;
     });
 
-    // initialise/define new peer and return it
+    
+
     function InitPeer(type) { 
-        //creating an instance of peer for new webrtc peer connection
         let peer = new Peer({initiator: (type == 'init') ? true : false, stream: stream, trickle: false});
-        peer.on('stream', function(stream) {  //when we get the stream from the other user,create new video
-            CreateVideo(stream);
+        peer.on('stream', function(stream) {  
+            CreateVideo(stream); 
         });
 
         //peer.on('close', function() {
             //document.getElementById("peerVideo").remove();
             //peer.destroy();
         //});
-        //listener when any data comes,here filter value is stored in decodeddata,this is for other client
-        peer.on('data', function(data) {
-            let decodedData = new TextDecoder('utf-8').decode(data);  //a specific character encoding, like utf-8
+        
+        peer.on('data', function(data) {  
+            let decodedData = new TextDecoder('utf-8').decode(data);  
             let peervideo = document.querySelector('#peerVideo');
             peervideo.style.filter = decodedData;
         })
@@ -8230,32 +8223,30 @@ navigator.mediaDevices.getUserMedia({video: true, audio: true}) //get video/voic
 
     //creating a peer of type init
     function MakePeer() {
-        client.gotAnswer = false;//initially the ans of client is set to false,then we send offer and wait for response
-        let peer = InitPeer('init');  //get peer reply back from this calling func
-        peer.on('signal',function(data) {  //init type will automatically run the signal func
+        client.gotAnswer = false;
+        let peer = InitPeer('init');  
+        peer.on('signal',function(data) {  
             if(!client.gotAnswer) {
                 socket.emit('Offer', data);
             }
         });
-        //if gotAnswer is true,
         client.peer = peer;
     }
 
-    //creating a peer of type notinit, when we get answer from another client,we shd send him answer
+    //creating a peer of type notinit
     function FrontAnswer(offer) {
         let peer = InitPeer('notInit');
-        peer.on('signal',(data) => {  //signal func won't run automatically,we have to call
-            socket.emit('Answer', data);  //event is answer
+        peer.on('signal',(data) => { 
+            socket.emit('Answer', data); 
         });
-        peer.signal(offer);  //pass the data(here offer) from 'signal' events to remote peer and call peer.signal(data) to get connected.
-        client.peer = peer;  //adding to client object
+        peer.signal(offer);  
+        client.peer = peer;  
     }
 
-    //when answer comes from backend
     function SignalAnswer(answer) {
         client.gotAnswer = true;
         let peer = client.peer;
-        peer.signal(answer);  //goes to line37
+        peer.signal(answer); 
     }
 
     function CreateVideo(stream) {
@@ -8263,13 +8254,12 @@ navigator.mediaDevices.getUserMedia({video: true, audio: true}) //get video/voic
         let video = document.createElement('video');
         video.id = "peerVideo";
         video.srcObject = stream;
-        //video.class = "embed-responsive-item";
         video.setAttribute('class', 'embed-responsive-item');
-        document.querySelector("#peerDiv").appendChild(video); //inside that div,video element is added as child
+        document.querySelector("#peerDiv").appendChild(video); 
         video.play();
         //wait for 1 sec
-        setTimeout(() => SendFilter(currentFilter), 1000);
-        video.addEventListener('click', () => {
+        setTimeout(() => SendFilter(currentFilter), 1000); 
+        video.addEventListener('click', () => { 
             if (video.volume != 0)
                 video.volume = 0
             else
@@ -8277,35 +8267,33 @@ navigator.mediaDevices.getUserMedia({video: true, audio: true}) //get video/voic
         })
     }
 
-    //when other person opens the url while session is going on,he shd be notified
     function SessionActive() {
         document.write('Session Active. Please come back later');
     }
 
     function SendFilter(filter) {
-        //check if ther is another person
         if(client.peer) {
-            client.peer.send(filter);  //built-in send() func
+            client.peer.send(filter);  
         }
     }
-
-    socket.on('BackOffer',FrontAnswer); //when offer comes from backend
-    socket.on('BackAnswer', SignalAnswer); //Hub will call this fucntion to send the answer ot the caller
+    
+    
+    
+    socket.on('BackOffer',FrontAnswer); 
+    socket.on('BackAnswer', SignalAnswer); 
     socket.on('SessionActive', SessionActive);
     socket.on('CreatePeer', MakePeer);
     socket.on('Disconnect', RemovePeer);
 
 })
-//if not,
 .catch(err => document.write(err));
-
-//npm run watch converts main.js to bundle.js
 
 checkboxTheme.addEventListener('click', () => {
     if (checkboxTheme.checked == true) {
         document.body.style.backgroundColor = '#212529'; //black
         if (document.querySelector('#muteText')) {
             document.querySelector('#muteText').style.color = "#fff";  //white
+            document.querySelector('#text-color').style.color = '#fff';
         }
 
     }
@@ -8313,11 +8301,11 @@ checkboxTheme.addEventListener('click', () => {
         document.body.style.backgroundColor = '#fff';
         if (document.querySelector('#muteText')) {
             document.querySelector('#muteText').style.color = "#212529";
+            document.querySelector('#text-color').style.color = '#212529';
         }
     }
 });
 
-//when video is creted,text is also dynamically created, and removed when video is removed
 function CreateDiv() {
     let div = document.createElement('div');
     div.setAttribute('class', "centered");
--- server.js
+++ server.js
@@ -1,20 +1,17 @@
-const express = require('express');
+const express = require('express');  
 const app = express();
-//app=require('express')();
-const http = require('http').Server(app); //supply app to server
-//create an instance pf socket.io by passing http
-const io = require('socket.io')(http);
-const port = process.env.PORT || 3000; //either 3000 or port available
+const http = require('http').Server(app);
+const io = require('socket.io')(http);  
+const port = process.env.PORT || 3000; 
 
-//load static files
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public")); 
 let clients = 0;
 
-io.on('connection', function(socket) {
-    socket.on('NewClient', function() {
+io.on('connection', function(socket) { 
+    socket.on('NewClient', function() { 
         if(clients < 2) {
             if(clients == 1) {
-                this.emit('CreatePeer'); //this particular socket runs the createpeer event which will run MakePeer func which sends the offer to first client
+                this.emit('CreatePeer'); 
             }
         }
         else {
@@ -22,25 +19,21 @@ io.on('connection', function(socket) {
         }
         clients++;
     })
-    //when offer comes from front-end,sendoffer will handle that
     socket.on('Offer',SendOffer);
-    //when ans is coming,send ans to other user
     socket.on('Answer',SendAnswer);
-    //when we close window
     socket.on('disconnect', Disconnect);
 })
 
 function Disconnect() {
     if (clients > 0) {
         if (clients <= 2)
-            this.broadcast.emit("Disconnect");//when one person disconnected,ask other person to remove video by sending event Disconnect to front end main.js
+            this.broadcast.emit("Disconnect");
         clients--;
     }
 }
 
-//data sent from main.js is offer parameter here,send offer to other user
 function SendOffer(offer) {
-    this.broadcast.emit('BackOffer', offer); //send to other users but not to us, backoffer is event name
+    this.broadcast.emit('BackOffer', offer); 
 }
 
 function SendAnswer(data) {
